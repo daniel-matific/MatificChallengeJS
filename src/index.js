26eac10802748dc343843361472b020d80c269ba
daniel-matific <31887605+daniel-matific@users.noreply.github.com>
2019-03-23 10:46:58 +0200

    Ratio adjustment on game height

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ let canvas = document.getElementById("gameScreen");
 let context = canvas.getContext("2d");
 
 const GAME_WIDTH = 800;
-const GAME_HEIGHT = 600;
+const GAME_HEIGHT = GAME_WIDTH * 0.75;
 
 canvas.width = GAME_WIDTH;
 canvas.height = GAME_HEIGHT;
