d1b58e05200f59e84d1ac113fe98106c854dfdbf
daniel-matific <31887605+daniel-matific@users.noreply.github.com>
2019-04-02 17:40:51 +0300

    Changed images source locations and build script

--- src/View/viewCfgEnum.js
+++ src/View/viewCfgEnum.js
@@ -1,11 +1,11 @@
 export const VIEW_CFG = {
   FONT_RATIO: 0.05,
 
-  BACKGROUND_IMAGE: "/src/resources/background.png",
+  BACKGROUND_IMAGE: "./resources/background.png",
   BACKGROUND_HEIGHT_RATIO: 0.66,
 
-  BOAT_IMAGE_LEFT: "/src/resources/boatLeft.png",
-  BOAT_IMAGE_RIGHT: "/src/resources/boatRight.png",
+  BOAT_IMAGE_LEFT: "./resources/boatLeft.png",
+  BOAT_IMAGE_RIGHT: "./resources/boatRight.png",
   BOAT_WIDTH_RATIO: 0.12,
   BOAT_HEIGHT_RATIO: 0.63,
   BOAT_YPOSITION_RATIO: 0.8,
@@ -15,19 +15,19 @@ export const VIEW_CFG = {
   DASHBOARD_YPOSITION_RATIO: 0.08,
   DASHBOARD_POSITION_RATIO: 0.08,
 
-  PARACHUTIST_IMAGE: "/src/resources/parachutist.png",
+  PARACHUTIST_IMAGE: "./resources/parachutist.png",
   PARACHUTIST_WIDTH_RATIO: 0.05,
   PARACHUTIST_HEIGHT_RATIO: 1.47,
   PARACHUTIST_SPEED: 0.0025,
   PARACHUTIST_EVENT_DROWNED: "parachutistDrowned",
   PARACHUTIST_EVENT_SAVED: "parachutistSaved",
 
-  PLANE_IMAGE: "/src/resources/plane.png",
+  PLANE_IMAGE: "./resources/plane.png",
   PLANE_WIDTH_RATIO: 0.15,
   PLANE_HEIGHT_RATIO: 0.78,
   PLANE_YPOSITION_RATIO: 0.04,
   PLANE_SPEED_RATIO: 0.003,
 
-  SEA_IMAGE: "/src/resources/sea.png",
+  SEA_IMAGE: "./resources/sea.png",
   SEA_HEIGHT_RATIO: 0.15
 };
